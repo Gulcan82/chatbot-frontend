e3d2950235b76f4366d1c42f4eac9d5587528985
Gülcan Kamat <gulcankamat@gmail.com>
2024-10-23 11:42:01 +0200

    Perfection the animation of Welcome to our project (AI).

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-
 import './Robot.css'; // Ensure you have this file for the styles
 
-import './Robot.css'; // Wenn du eigene Styles für den Roboter hast, kannst du sie hier importieren.
 
 
 const Robot = () => {
@@ -12,19 +10,13 @@ const Robot = () => {
     {/* Video player replacing the image */}
     <video 
       src={`${process.env.PUBLIC_URL}/Abschluss-projekt .mp4`} // Replace with your actual video file path
-      autoPlay 
-      loop 
-      muted
+      autoPlay
       className="robot-video" // Add a class for styling purposes
     />
     <div className="robot-eyes"></div>
-      <p>WIlkommen An Unsere Abschluss Projekt  </p>
+      <p>Willkommen zu unserem Abschluss Projekt</p>
 
-      {/* Passe den Bildpfad an die tatsächliche Speicherort-Struktur an */}
-      <img src={`${process.env.PUBLIC_URL}/robota-image.png`} alt="Robot Assistant" className="robot-image" />
-      <div className="robot-eyes"></div>
-      <p>Ich bin dein Roboter-Assistent!</p>
-
+      
     </div>
   );
 };
